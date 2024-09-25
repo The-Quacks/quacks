5b72cc2360af50ee3342a5450da06863f77eb310
msrnic <msrnic@ualberta.ca>
2024-09-25 17:38:48 -0600

    Color Prepared

--- app/src/main/java/com/example/quacks/Shape.java
+++ app/src/main/java/com/example/quacks/Shape.java
@@ -3,5 +3,9 @@ package com.example.quacks;
 public abstract class Shape {
     private int x;
     private int y;
+<<<<<<< HEAD
     String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+=======
+    String color = "blue";
+>>>>>>> b81abdb (Color Prepared)
 }
