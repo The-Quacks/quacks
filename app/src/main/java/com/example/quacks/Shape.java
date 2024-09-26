a10d7d9ecfc80e00998be8f772cb6bb1f746a613
msrnic <msrnic@ualberta.ca>
2024-09-25 18:26:13 -0600

    Fix Again

--- app/src/main/java/com/example/quacks/Shape.java
+++ app/src/main/java/com/example/quacks/Shape.java
@@ -3,9 +3,5 @@ package com.example.quacks;
 public abstract class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
     String color = "$PUT_YOUR_CHOICE_OF_COLOR";
-=======
-    String color = "blue";
->>>>>>> b81abdb (Color Prepared)
 }
